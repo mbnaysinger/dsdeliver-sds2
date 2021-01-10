c7eae1aae1c474d03183da579b856c7633d38189
Maike Borges <maikenborges@gmail.com>
2021-01-10 17:45:48 -0300

    Part 2 Project Finalized

--- front-web/src/Orders/StepsHeader.tsx
+++ front-web/src/Orders/StepsHeader.tsx
@@ -1,8 +1,8 @@
 import './styles.css';
 function StepsHeader() {
     return (
-        <header className="order-steps-container">
-            <div className="order-steps-content">
+        <header className="orders-steps-container">
+            <div className="orders-steps-content">
                 <h1 className="steps-title">
                     SIGA AS <br /> ETAPAS
                 </h1>
---
+++ front-web/src/Orders/types.ts
@@ -0,0 +1,9 @@
+import { NoSubstitutionTemplateLiteral } from "typescript";
+
+export type Product = {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    imageUri: string;
+}
