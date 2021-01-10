08308a2739f25242467bf3f9e5ce16a0fc582c89
Maike Borges <maikenborges@gmail.com>
2021-01-10 20:48:34 -0300

    Search order location

--- front-web/src/Orders/api.ts
+++ front-web/src/Orders/api.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
 
 const API_URL = 'http://localhost:8080';
+const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
     return axios(`${API_URL}/products`)
+
+}
+
+export function fetchLocalMapBox(local: string) {
+    return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=pk.eyJ1IjoibWJuYXlzaW5nZXIiLCJhIjoiY2tqcm9hMHk5NmY5YzJ4cDlpanJoa3ZtNiJ9.3fA4BSLWXdYOqfNOrw0odg`)
 
 }
--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -1,9 +1,13 @@
-import { NoSubstitutionTemplateLiteral } from "typescript";
-
 export type Product = {
     id: number;
     name: string;
     price: number;
     description: string;
     imageUri: string;
+}
+
+export type OrderLocationdata = {
+    address: string,
+    latitude: number,
+    longitude: number,
 }
